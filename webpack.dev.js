596584e969e173fdcd96ff256eac345248fe6dc1
Elena Nedkova <lenuza@gmail.com>
2020-05-16 21:43:10 +0300

    added webpack bundle analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
@@ -36,7 +37,8 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-    })
+        }),
+        new BundleAnalyzerPlugin()
     ]
     // output: { ...output }
 };
